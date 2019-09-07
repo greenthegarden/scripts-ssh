33f9c2083cbe16e6d4d394d25aa6316af9c78de7
Philip Cutler <greenthegarden@gmail.com>
2019-09-07 20:02:02 +0930

    improved key creation

--- create_ssh_key.sh
+++ create_ssh_key.sh
@@ -8,7 +8,7 @@ function create_key {
   echo "generating key file ${key_file_dir}/${key_file} using ${key_algorithm} algorithm with size ${key_size}"
 
   ssh-keygen -t ${key_algorithm} -b ${key_size} "${key_comment_arg}" -f "${key_file_dir}/${key_file}"
-  export ${key_file}
+  export key_file
 }
 
 read -p "Enter key algorithm [${key_algorithm}]: " key_algorithm_input
@@ -31,7 +31,9 @@ key_file=${key_file_input:-${key_file}}
 read -p "Enter key file directory [${key_file_dir}]: " key_file_dir_input
 key_file_dir=${key_file_dir_input:-${key_file_dir}}
 
-recreate="yes"
-if [ -f ${key_file_dir}/${key_file} ] ; then
-  create_key
+if [ ! -d ${key_file_dir} ] ; then
+  mkdir -p ${key_file_dir}
 fi
+
+create_key
+
