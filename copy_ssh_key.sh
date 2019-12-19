e77666bb59b1aa3558eb632cb7f30418910db8ca
Philip Cutler <greenthegarden@gmail.com>
2019-12-19 05:34:48 +0000

    enable support for array of remote hosts

--- copy_ssh_key.sh
+++ copy_ssh_key.sh
@@ -4,6 +4,12 @@ source ./key_vars.sh
 
 remote_user=${USER}
 
+IFS=$' \t\n'
+
+# define list as a string which contains all hosts key should be copied to
+# string is converted to array later in script
+remote_hosts_list="localhost"
+
 read -p "Enter key file name [${key_file}]: " key_file_input
 key_file=${key_file_input:-${key_file}}
 
@@ -11,16 +17,25 @@ read -p "Enter key file directory [${key_file_dir}]: " key_file_dir_input
 key_file_dir=${key_file_dir_input:-${key_file_dir}}
 
 if [ -f ${key_file_dir}/${key_file} ] ; then
-  read -p "Enter remote host [localhost]: " remote_host_input
-  remote_host=${remote_host_input:-localhost}
 
-  read -p "Enter remote user [${remote_user}]: " remote_user_input
+  read -p "Enter remote user for hosts [${remote_user}]: " remote_user_input
   remote_user=${remote_user_input:-${remote_user}}
 
-  echo "copying key file ${key_file_dir}/${key_file} to ${remote_user}@${remote_host}"
-  ssh-copy-id -i ${key_file_dir}/${key_file} ${remote_user}@${remote_host}
+  read -p "Enter remote hosts (as space separated list) [${remote_hosts_list}]: " remote_hosts_list_input
+  echo "${remote_hosts_list_input}"
+  remote_hosts_list=${remote_hosts_list_input:-${remote_hosts_list}}
+  echo ${remote_hosts_list}
+
+  # convert hosts list to array
+  remote_hosts=( ${remote_hosts_list} )
+  echo ${remote_hosts[*]}
+
+  for remote_host in "${remote_hosts[@]}"; do
+    echo "Copying key file ${key_file_dir}/${key_file} to ${remote_user}@${remote_host}"
+    ssh-copy-id -i ${key_file_dir}/${key_file} ${remote_user}@${remote_host}
+  done
+
 else
-  echo "key file ${key_file_dir}/${key_file} not found!"
+  echo "Key file ${key_file_dir}/${key_file} not found!"
   exit
 fi
-
